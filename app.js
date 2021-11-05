770c595f72c0f615eac7f5d6a7d258836f42e0b2
Jamie Coleman <jamiecoleman@Jamies-MacBook-Pro.local>
2021-11-05 10:30:04 -0500

    Created individual tiles in game area

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Set tile size and dimensions of the game area
 const TILE = 60;
 const gridWidth = 10 * TILE;
 const gridHeight = 15 * TILE;
@@ -6,3 +7,16 @@ const gameGrid = document.querySelector('#gameGrid');
 gameGrid.style.width = gridWidth + "px";
 gameGrid.style.height = gridHeight + "px";
 
+// Create individual tiles in the game area
+// Store access to the individual tiles in gridTileArr
+const gridTileArr = []
+
+for(let i = 0; i < 150; i++) {
+    let gridTile = document.createElement('div');
+    gridTile.id = i;
+    gridTile.style.width = TILE + "px";
+    gridTile.style.height = TILE + "px";
+    gridTile.textContent = i;
+    gameGrid.append(gridTile);
+    gridTileArr.push(gridTile);
+}
