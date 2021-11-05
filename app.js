588902e058312404e816890cd7c3ca894286db0b
Jamie Coleman <jamiecoleman@Jamies-MacBook-Pro.local>
2021-11-05 10:15:22 -0500

    Defined gameGrid with tile sizes

---
+++ app.js
@@ -0,0 +1,8 @@
+const TILE = 60;
+const gridWidth = 10 * TILE;
+const gridHeight = 15 * TILE;
+
+const gameGrid = document.querySelector('#gameGrid');
+gameGrid.style.width = gridWidth + "px";
+gameGrid.style.height = gridHeight + "px";
+
