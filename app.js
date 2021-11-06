3abcaefe3e98921bcd4127ad1f5171ef288e34c9
Jamie Coleman <jamiecoleman@Jamies-MacBook-Pro.local>
2021-11-05 23:06:57 -0500

    Refined rotation around fixed point

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function setupGame(game) {
     // Calculates and sets the width and height of playable area in px, including tile borders using the game object
     game.gridSelector = document.querySelector('#gameGrid');
     game.gridWidth = game.tilesWide * game.tileDimension + 2 * game.tilesWide;
-    game.gridHeight = game.tilesHigh * game.tileDimension + 2 * game.tilesWide;
+    game.gridHeight = game.tilesHigh * game.tileDimension + 2 * game.tilesHigh;
     game.gameWidth = game.gridWidth + "px";
     game.gridSelector.style.width = game.gameWidth;
     game.gameHeight = game.gridHeight + "px";
@@ -52,6 +52,7 @@ class Tetroid {
         this.curOrientation = 0;
     }
     
+    // Creates each new tetroid
     initialPos() {
         this.curOrientation = 0;
         this.curPosTiles = this.vers0.slice();
@@ -61,6 +62,7 @@ class Tetroid {
         })
     }
     
+    // Updates position of tetroid by arrow keys and gravity
     updatePos(shiftBy) {
         this.curPosTiles.forEach((tilePos, index) => {
             game.tileArr[tilePos].style.backgroundColor = 'black';
@@ -72,7 +74,7 @@ class Tetroid {
         })
     }
 
-    // Method to select a rotational orientation of the tetroid
+    // Selects a rotational orientation of the tetroid
     getVersion(versionNumber) {
         if (versionNumber == 0) {
             return this.vers0.slice();
@@ -91,12 +93,12 @@ class Tetroid {
         }
     }
 
+    // Rotates tetroid around a fixed point
     rotateTetroid() {
         this.curPosTiles.forEach((tilePos) => {
             game.tileArr[tilePos].style.backgroundColor = 'black';
         })
-        console.log(this.curPosTiles)
-        console.log(this.getVersion(this.curOrientation))
+
         let shiftBy = this.curPosTiles[0] - this.getVersion(this.curOrientation)[0];
         this.curOrientation += 1;
         this.curOrientation = this.curOrientation % 4;
@@ -114,24 +116,25 @@ class Tetroid {
 const lineShape = new Tetroid(0, [2, 12, 22, 32], [10, 11, 12, 13], [2, 12, 22, 32], [10, 11, 12, 13]);
 game.shapeTemplates.push(lineShape);
 
-const lShape = new Tetroid(1, [1, 11, 21, 22], [11, 12, 13, 21], [1, 2, 12, 22], [3, 11, 12, 13]);
+const lShape = new Tetroid(1, [1, 2, 12, 22], [11, 12, 13, 21], [1, 11, 21, 22], [3, 11, 12, 13]);
 game.shapeTemplates.push(lShape);
 
-const revLShape = new Tetroid(2, [1, 11, 21, 20], [10, 11, 12, 0], [1, 11, 21, 2], [0, 1, 2, 12])
+const revLShape = new Tetroid(2, [2, 12, 21, 22], [11, 12, 13, 1], [2, 12, 22, 3], [11, 12, 13, 23])
 game.shapeTemplates.push(revLShape);
 
-const tShape = new Tetroid(3, [10, 11, 12, 21], [10, 1, 11, 21], [10, 11, 12, 1], [0, 10, 20, 11]);
+const tShape = new Tetroid(3, [2, 12, 22, 13], [11, 12, 13, 22], [2, 12, 22, 11], [11, 12, 13, 2]);
 game.shapeTemplates.push(tShape);
 
-const squareShape = new Tetroid(4, [0, 1, 10, 11], [0, 1, 10, 11], [0, 1, 10, 11], [0, 1, 10, 11]);
+const squareShape = new Tetroid(4, [1, 2, 11, 12], [1, 2, 11, 12], [1, 2, 11, 12], [1, 2, 11, 12]);
 game.shapeTemplates.push(squareShape);
 
-const sShape = new Tetroid(5, [1, 2, 10, 11], [0, 10, 11, 21], [1, 2, 10, 11], [0, 10, 11, 21]);
+const sShape = new Tetroid(5, [2, 3, 11, 12], [1, 11, 12, 22], [2, 3, 11, 12], [1, 11, 12, 22]);
 game.shapeTemplates.push(sShape);
 
-const revSShape = new Tetroid(6, [0, 1, 11, 12], [1, 10, 11, 20], [0, 1, 11, 12], [1, 10, 11, 20]);
+const revSShape = new Tetroid(6, [1, 2, 12, 13], [2, 11, 12, 21], [1, 2, 12, 13], [2, 11, 12, 21]);
 game.shapeTemplates.push(revSShape);
 
+// Randomly selects next tetroid to appear
 function generateShape() {
     game.curTemplateId = Math.floor(Math.random() * game.shapeTemplates.length);
 
@@ -168,36 +171,26 @@ function generateShape() {
 }
 
 generateShape()
+
 document.addEventListener('keydown', (event) => {
-    //rotate tetroid if spacebar clicked
     switch (event.code) {
+        // Rotate tetroid if spacebar clicked
         case "Space":
             game.shapeTemplates[game.curTemplateId].rotateTetroid();
-            console.log("Spacebar pressed");
             break;
+        // Press left arrow to move tetroid left
         case "ArrowLeft":
             game.shapeTemplates[game.curTemplateId].updatePos(-1);
-            console.log("Left arrow pressed");
             break;
+        // Press right arrow to move tetroid right
         case "ArrowRight":
             game.shapeTemplates[game.curTemplateId].updatePos(1);
-            console.log("Right arrow pressed");
+            break;
+        // Press down arrow to drop tetroid
+        case "ArrowDown":
+            game.shapeTemplates[game.curTemplateId].updatePos(10);
             break;
     }
-})
+}) 
 
-
-/* testShape = 0
-
-game.shapeTemplates[testShape].getVersion(0).forEach(tilePos => {
-    game.tileArr[tilePos].style.backgroundColor = 'red';
-})
-game.shapeTemplates[testShape].getVersion(1).forEach(tilePos => {
-    game.tileArr[tilePos + 5].style.backgroundColor = 'red';
-})
-game.shapeTemplates[testShape].getVersion(2).forEach(tilePos => {
-    game.tileArr[tilePos + 50].style.backgroundColor = 'red';
-})
-game.shapeTemplates[testShape].getVersion(3).forEach(tilePos => {
-    game.tileArr[tilePos + 55].style.backgroundColor = 'red';
-}) */
+//game.shapeTemplates[game.curTemplateId = 3].initialPos()
